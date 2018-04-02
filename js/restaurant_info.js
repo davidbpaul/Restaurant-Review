82c811ed9f681e51ea9d05ec23045dea129d8a3c
David Paul <david-paul23@hotmail.com>
2018-04-02 00:36:54 -0400

    update

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name;
+  image.alt = `An image of ${restaurant.name} in Manhattan.`;
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
--- sw.js
+++ sw.js
@@ -1,42 +1,55 @@
+const files = [
+    '/',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/restaurant.html',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/js/main.js',
+
+];
+
+const staticCacheName = 'restaurant-static-v2';
+
+
+//cache files
 self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open("restaurantCache")
-      .then(function (cache) {
-        return cache.addAll(
-          [
-            '/',
-            './img/1.jpg',
-            './img/2.jpg',
-            './img/3.jpg',
-            './img/4.jpg',
-            './img/5.jpg',
-            './img/6.jpg',
-            './img/7.jpg',
-            './img/8.jpg',
-            './img/9.jpg',
-            './img/10.jpg',
-            './index.html',
-            './restaurant.html',
-            './js/restaurant_info.js',
-            './css/styles.css',
-            './js/dbhelper.js',
-            './js/main.js',
-            './data/restaurants.json'
-
-          ]
-        );
+    event.waitUntil(
+      caches.open(staticCacheName).then(function (cache) {
+          return cache.addAll(files)
       })
-  );
+    );
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
+
+//update files
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+          return Promise.all(cacheNames.map(cacheName => {
+            if(cacheName !== staticCacheName) {
+                caches.delete(cacheName);
+            }
+          }))
+
+        })
+    );
+})
+
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
     .catch(e => console.log(e))
