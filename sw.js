53de122e27d3a62385dde55d9765b9c45ee0d9b7
David Paul <david-paul23@hotmail.com>
2018-04-02 00:38:13 -0400

    fix

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const files = [
-    '/',
-    '/css/styles.css',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/restaurant.html',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
-    '/js/main.js',
+  '/',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/restaurant.html',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+  '/js/main.js',
 
 ];
 
@@ -24,26 +24,26 @@ const staticCacheName = 'restaurant-static-v2';
 
 //cache files
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-      caches.open(staticCacheName).then(function (cache) {
-          return cache.addAll(files)
-      })
-    );
+  event.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(files)
+    })
+  );
 });
 
 
 //update files
 self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-          return Promise.all(cacheNames.map(cacheName => {
-            if(cacheName !== staticCacheName) {
-                caches.delete(cacheName);
-            }
-          }))
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(cacheName => {
+        if(cacheName !== staticCacheName) {
+          caches.delete(cacheName);
+        }
+      }))
 
-        })
-    );
+    })
+  );
 })
 
 
